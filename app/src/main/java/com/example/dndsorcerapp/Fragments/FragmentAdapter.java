a042edc984847c44639bc9bf802b810d1f3c60c7
damon_estrada <damon.estrada@cs.nmt.edu>
2019-09-08 19:18:56 -0600

    Supports deletion of fragments from the ViewPager. Does so with help of notify DataSetChanged() & getItemPostion().

--- app/src/main/java/com/example/dndsorcerapp/Fragments/FragmentAdapter.java
+++ app/src/main/java/com/example/dndsorcerapp/Fragments/FragmentAdapter.java
@@ -1,16 +1,25 @@
 package com.example.dndsorcerapp.Fragments;
 
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager.widget.PagerAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class FragmentAdapter extends FragmentStatePagerAdapter {
 
-    private final List<Fragment> mFragmentList = new ArrayList<>();
-    private final List<String> mFragmentTitleList = new ArrayList<>();
+    private  List<Fragment> mFragmentList = new ArrayList<>();
+    private  List<String> mFragmentTitleList = new ArrayList<>();
+
+    public void setmFragmentList(List<Fragment> mFragmentList) {
+        this.mFragmentList = mFragmentList;
+    }
 
     public FragmentAdapter(FragmentManager fm) {
         super(fm);
@@ -21,11 +30,6 @@ public class FragmentAdapter extends FragmentStatePagerAdapter {
         return mFragmentList.size();
     }
 
-    public void addFragment(Fragment fragment, String title) {
-        mFragmentList.add(fragment);
-        mFragmentTitleList.add(title);
-    }
-
     @Override
     public Fragment getItem(int position) {
         return mFragmentList.get(position);
@@ -34,5 +38,28 @@ public class FragmentAdapter extends FragmentStatePagerAdapter {
     @Override
     public float getPageWidth(int position) {
         return super.getPageWidth(position);
+    }
+
+    public void addFragment(Fragment fragment, String title) {
+        mFragmentList.add(fragment);
+        mFragmentTitleList.add(title);
+        notifyDataSetChanged();
+    }
+
+    public void deleteFragment(int position) {
+        if (mFragmentList.size() > 0) {
+            mFragmentList.remove(position);
+            notifyDataSetChanged();
+        }
+    }
+
+    /**
+     * This is called after deleteFragment (when notifyDataSetChanged() is called ).
+     * @param object
+     * @return
+     */
+    @Override
+    public int getItemPosition(@NonNull Object object) {
+        return PagerAdapter.POSITION_NONE;
     }
 }
